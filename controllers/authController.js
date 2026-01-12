d574f66f3b458d8ca6b4ba8f1261d5fa72948243
mohfadel2022 <mohfadel2021@gmail.com>
2026-01-12 18:58:28 +0100

    Fix - cookie: sameSite:'none'

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,7 +22,7 @@ exports.login = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
+            sameSite: 'none',
             maxAge: maxAge
         });
 
